d945133832f8dfb993f7c13fa5a5ab8b3de281a7
chunkingz <fortuneking33@yahoo.com>
2019-07-07 19:28:25 +0100

    :wrench: code refactor for code climate

--- server/helpers/validator.js
+++ server/helpers/validator.js
@@ -1,15 +1,20 @@
 import { check } from 'express-validator';
 
-const refactor = (a, b) => check(a)
+const refactor1 = (a, b) => check(a)
   .not().isEmpty().withMessage(`${a} is required`)
   .isInt()
   .withMessage(`${a} must be a valid number`)
   .isLength({ min: 3 })
   .withMessage(b);
 
+const refactor2 = (a, b) => check(a)
+  .not().isEmpty().withMessage(`${a} is required`)
+  .isLength({ min: 3 })
+  .withMessage(`${a} should be ${b} characters or more`);
+
 const postValidate = [
   check('owner', 'Owner should be set to a valid email').isEmail(),
-  refactor('price', 'invalid price amount'),
+  refactor1('price', 'invalid price amount'),
   check('state', 'State is required and should be more than 3 chars').isLength({ min: 3 }),
   check('city', 'City is required and should be more than 3 chars').isLength({ min: 3 }),
   check('address', 'Address is required and should be more than 5 chars').isLength({ min: 5 }),
@@ -18,31 +23,16 @@ const postValidate = [
 
 const userSignUpValidate = [
   check('email', 'Email must be valid').isEmail(),
-  check('first_name')
-    .not().isEmpty().withMessage('First name is required')
-    .isLength({ min: 3 })
-    .withMessage('First name should be 3 characters or more'),
-  check('last_name')
-    .not().isEmpty().withMessage('Last name is required')
-    .isLength({ min: 3 })
-    .withMessage('Last name should be 3 characters or more'),
-  check('password')
-    .not().isEmpty().withMessage('Password is required')
-    .isLength({ min: 3 })
-    .withMessage('Password should be 5 characters or more'),
-  refactor('phoneNumber', 'Phone Number should be 11 characters'),
-  check('address')
-    .not().isEmpty().withMessage('Address is required')
-    .isLength({ min: 3 })
-    .withMessage('Address should be 5 characters or more'),
+  refactor2('first_name', 3),
+  refactor2('last_name', 3),
+  refactor2('password', 5),
+  refactor1('phoneNumber', 'Phone Number should be 11 characters'),
+  refactor2('address', 5),
 ];
 
 const userSignInValidate = [
   check('email', 'Email must be valid').isEmail(),
-  check('password')
-    .not().isEmpty().withMessage('Password is required')
-    .isLength({ min: 3 })
-    .withMessage('Password should be 5 characters or more'),
+  refactor2('password', 5),
 ];
 
 
